535ac30d1b43a8708accd3073a66b6d1f606b34d
thaninpong <thaninpong@perfenterprise.com>
2018-06-22 15:29:22 +0700

    add project

---
+++ ConsoleApp1/ImplementClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class ImplementClass
+    {
+        public string ImplementTest()
+        {
+            return null; 
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("loop : ");
+            int n = Int32.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i=1;i<=n;i++)
+            {
+                Console.Write("Number "+i+":");
+                int input = Int32.Parse(Console.ReadLine());
+                sum += input;
+            }
+            Console.WriteLine(sum);
+
+
+            while (true) {
+
+            Console.Write("Stop");
+            string xxx = Console.ReadLine();
+            if (xxx == "y")
+            {
+                int sum2 = 0;
+                Console.Write("loop : ");
+                int n2 = Int32.Parse(Console.ReadLine());
+                for (int i = 1; i <= n2; i++)
+                {
+                    Console.Write("Number " + i + ":");
+                    int input2 = Int32.Parse(Console.ReadLine());
+                    sum2 += input2;
+                }
+                    Console.WriteLine(sum2);
+                }
+                else {
+                Console.WriteLine("Buy");break;
+            }
+            }
+
+        }
+    }
+}
